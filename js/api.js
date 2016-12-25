ae6c087df8df73b7fd6d64b9ea8db3de6734e176
fengzhenhai <zhenhai.fzh@alibaba-inc.com>
2016-12-25 19:35:50 +0800

    实现跑马灯效果

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
 exports.urls = { // 在外部js文件中定义
     base_url:"http://192.168.1.104:8080/",
-    icon_base_url:"http://192.168.1.104:8080/icon/"
+    icon_base_url:"http://192.168.1.104:8080/icon/",
+    house_url:"http://192.168.1.104:8080/icon/house_2_96px.png"
 }
