f67802392b07bb255727afcc3bba248918826e33
fengzhenhai <zhenhai.fzh@alibaba-inc.com>
2016-12-28 14:47:41 +0800

    将css文件提出来

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 exports.urls = { // 在外部js文件中定义
-    base_url:"http://192.168.1.104:8080/",
-    icon_base_url:"http://192.168.1.104:8080/icon/",
-    house_url:"http://192.168.1.104:8080/icon/house_2_96px.png"
+    base_url:"http://30.96.224.66:8080/",
+    icon_base_url:"http://30.96.224.66:8080/icon/",
+    house_url:"http://30.96.224.66:8080/icon/house_2_96px.png"
 }
